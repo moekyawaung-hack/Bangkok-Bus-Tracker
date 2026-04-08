78996ab436924100b072ae33c05eeeed5d8bc63a
moekyawaung-hack <moekyawaung@hackermail.com>
2026-04-08 12:37:15 +0630

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+LANGUAGES = ["en", "mm", "th"]
+
+DEFAULT_LANGUAGE = "en"
+
+GTFS_REALTIME_URL = "https://api.gtfs.bangkok.example/realtime"
+GTFS_STATIC_PATH = "data/gtfs/"
+
+OFFLINE_MAP_FILE = "data/maps/bangkok.mbtiles"
